c8c9d39105b005eb5f5405c205637e6a392027a3
wanittop <wanit.dan.it57@cpc.ac.th>
2016-11-05 11:39:16 +0700

    InwWanit

---
+++ app/src/main/java/com/example/os/week8su1/Lowlist_Activity.java
@@ -0,0 +1,13 @@
+package com.example.os.week8su1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lowlist_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lowlist_);
+    }
+}
